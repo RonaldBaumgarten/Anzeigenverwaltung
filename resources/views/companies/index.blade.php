f3114a6ce3fc58cbc978cbadaefcb579c5ed42d0
Ronald Baumgarten <baumgartendj@gmail.com>
2025-09-10 17:06:52 +0200

    change company index-view

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -3,55 +3,36 @@
     <x-slot:heading>
         Firmen
     </x-slot:heading>
-
     <x-slot:items>
-        @if (Auth::user())
-        <p><a href="{{ route('companies.create') }}" class="btn btn-primary">Neue Firma anlegen</a></p>
-        @else
-        <p><a href="{{ route('login') }}" class="btn btn-info">Einloggen, um neue Firma anzulegen</a></p>
-        @endif
-
-        <div class="container-lg">
-            @foreach($companies as $company)
+        <div class="container-lg" style="max-width: 800px">
             <div class="row mb-3"> 
-                <div class="col-sm-4 themed-grid-col"><strong>{{ $company->companyName }}</strong></div> 
-                <div class="col-sm-4 themed-grid-col mx-auto">
-                    <a href="{{ route('companies.show', $company) }}" class="btn btn-outline-primary">Details ansehen</a>
-                </div> 
-                @if (Auth::user())
-                <div class="col-md-2 themed-grid-col">
-                    <a href="{{ route('companies.edit', $company) }}" class="btn btn-outline-primary">Bearbeiten</a>
-                </div> 
-                <div class="col-lg-2 themed-grid-col">
+                <div class="col"> 
+                    @if (Auth::user())
+                    <p><a href="{{ route('companies.create') }}" class="btn btn-primary">Neue Firma anlegen</a></p>
+                    @else
+                    <p><a href="{{ route('login') }}" class="btn btn-info">Einloggen, um neue Firma anzulegen</a></p>
+                    @endif
+                </div>
+            </div>
+        </div>
+        <div class="container-lg" style="max-width: 800px">
+            <div class="row mb-3"> 
+            @foreach($companies as $company)
+                <div class="feature col-lg-6 themed-grid-col">
+                    <h3 class="fs-2 text-body-emphasis">{{ $company->companyName }}</h3>
+                    <a href="{{ route('companies.show', $company) }}" class="btn btn-primary">Details ansehen</a>
+                    @if (Auth::user())
+                    <a href="{{ route('companies.edit', $company) }}" class="btn btn-primary">Bearbeiten</a>
                     <form action="{{ route('companies.destroy', $company) }}" method="POST" style="display:inline;">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-outline-warning">Löschen</button>
+                    <button type="submit" class="btn btn-danger">Löschen</button>
+                    @endif
                 </div> 
-                @endif
-            </div>
             @endforeach
+            </div>
         </div>
 
-
-<!--
-        <ul class="list-group list-group-hover">
-            @foreach($companies as $company)
-                <li class="list-group-item">
-                        <strong>{{ $company->companyName }}</strong>
-                        <a href="{{ route('companies.show', $company) }}" class="btn btn-outline-primary">Details ansehen</a>
-                        @if (Auth::user())
-                        <a href="{{ route('companies.edit', $company) }}" class="btn btn-outline-primary">Bearbeiten</a>
-                        <form action="{{ route('companies.destroy', $company) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-outline-warning">Löschen</button>
-                        @endif
-                    </form>
-                </li>
-            @endforeach
-        </ul>
--->
     </x-slot:items>
 
 </x-index>
